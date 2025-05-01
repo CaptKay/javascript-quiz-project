7b3db48f4928b52e43fc2800c2ec3d026d1c290a
Kanu Kingsley <suge.kanu@gmail.com>
2025-05-01 21:41:33 +0200

    day 1 quiz project done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  constructor(text, choices, answer, difficulty) {
+    this.text = text; //string
+    this.choices = choices; // Array of strings
+    this.answer = answer; //string
+    this.difficulty = difficulty; //number
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    // this.choices = this.choices.sort(() => Math.random() - 0.5);
+    for (let i = this.choices.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // Generate a new random index
+        [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]; // Swap the elements at indices i and j
+    }
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions; // Array of Question objects
+    this.timeLimit = timeLimit; // Number
+    this.timeRemaining = timeRemaining; // Number
+    this.correctAnswers = 0; // Number
+    this.currentQuestionIndex = 0; // Number
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex]; // Returns the current question object
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++; // Increments the current question index
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // Generate a new random index
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ]; // Swap the elements at indices i and j
+    }
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (this.getQuestion().answer === answer) {
+      // Checks if the answer is correct
+      this.correctAnswers++; // Increments the correct answers count
+      return;
+    } 
+    return 'Wrong answer!'; // Returns a message if the answer is wrong
+  }
+
+  hasEnded() {
+    // return this.currentQuestionIndex >= this.questions.length; // Checks if the quiz has ended
+
+    if (this.currentQuestionIndex >= this.questions.length) {
+      return true; // Returns true if the quiz has ended
+    }
+    return false; // Returns false if the quiz has not ended
+  }
 }
